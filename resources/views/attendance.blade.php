10ae3c90611ff6fa4bc56fc484786e6b5b8a3a05
ryamao <yamao.ryouta@gmail.com>
2024-01-31 20:52:56 +0900

    feat(attendance): 日付別勤怠ページのビューを作成

--- resources/views/attendance.blade.php
+++ resources/views/attendance.blade.php
@@ -1,26 +1,30 @@
-@php
-use Carbon\CarbonImmutable;
-@endphp
-
 <x-app-layout>
-    <a href="{{ route('attendance', ['date' => $currentDate->subDay()->format('Y-m-d')]) }}" dusk="previous-date">&lt;</a>
-    <div dusk="current-date">{{ $currentDate->format('Y-m-d') }}</div>
-    <a href="{{ route('attendance', ['date' => $currentDate->addDay()->format('Y-m-d')]) }}" dusk="next-date">&gt;</a>
-    <table>
-        <thead>
-            <tr>
-                <th>名前</th>
-                <th>勤務開始</th>
-                <th>勤務終了</th>
-                <th>休憩時間</th>
-                <th>勤務時間</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($attendances as $attendance)
-            <x-attendance-row :$attendance />
-            @endforeach
-        </tbody>
-    </table>
-    {{ $attendances->links() }}
+    <x-slot name="css">attendance.css</x-slot>
+
+    <div class="attendance">
+        <div class="attendance__date">
+            <a href="{{ route('attendance', ['date' => $currentDate->subDay()->format('Y-m-d')]) }}" dusk="previous-date">&lt;</a>
+            <span class="attendance__current-date" dusk="current-date">{{ $currentDate->format('Y-m-d') }}</span>
+            <a href="{{ route('attendance', ['date' => $currentDate->addDay()->format('Y-m-d')]) }}" dusk="next-date">&gt;</a>
+        </div>
+
+        <table class="attendance__table">
+            <thead>
+                <tr>
+                    <th>名前</th>
+                    <th>勤務開始</th>
+                    <th>勤務終了</th>
+                    <th>休憩時間</th>
+                    <th>勤務時間</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($attendances as $attendance)
+                <x-attendance-row :$attendance />
+                @endforeach
+            </tbody>
+        </table>
+
+        <div class="attendance__pagination">{{ $attendances->links() }}</div>
+    </div>
 </x-app-layout>
--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -7,6 +7,9 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Atte</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/app-layout.css') }}" />
     @isset($css)
