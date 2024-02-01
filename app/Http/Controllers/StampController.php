930fb09ab41fd80380e3934781200b400b765d8b
ryamao <yamao.ryouta@gmail.com>
2024-02-01 16:56:16 +0900

    refactor: StampControllerのメソッドを整理

--- app/Http/Controllers/StampController.php
+++ app/Http/Controllers/StampController.php
@@ -11,18 +11,20 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
-use App\WorkStatus;
 
 /** 打刻ページと打刻機能のコントローラ */
 class StampController extends Controller
 {
     /** 打刻ページを表示する。 */
-    public function index(DateTimeZone $timezone): View
+    public function index(): View
     {
-        $this->stamper()->handlePreviousEvents();
+        $now = CarbonImmutable::now(app()->make(DateTimeZone::class));
+        $service = new StampService(Auth::user(), $now);
+        $service->handlePreviousEvents();
 
+        /** @var \App\Models\User */
         $user = Auth::user();
-        $now = CarbonImmutable::now($timezone);
+
         return view('stamp', [
             'userName' => $user->name,
             'workStatus' => $user->workStatus($now),
@@ -32,36 +34,38 @@ class StampController extends Controller
     /** 勤務開始処理を行う。 */
     public function storeShiftBegin(): RedirectResponse
     {
-        $this->stamper()->beginShift();
-        return redirect()->route('stamp');
+        return $this->storeStamp(fn (StampService $service) => $service->beginShift());
     }
 
     /** 勤務終了処理を行う。 */
     public function storeShiftTiming(): RedirectResponse
     {
-        $this->stamper()->endShift();
-        return redirect()->route('stamp');
+        return $this->storeStamp(fn (StampService $service) => $service->endShift());
     }
 
     /** 休憩開始処理を行う。 */
     public function storeBreakBegin(): RedirectResponse
     {
-        $this->stamper()->beginBreak();
-        return redirect()->route('stamp');
+        return $this->storeStamp(fn (StampService $service) => $service->beginBreak());
     }
 
     /** 休憩終了処理を行う。 */
     public function storeBreakTiming(): RedirectResponse
     {
-        $this->stamper()->endBreak();
-        return redirect()->route('stamp');
+        return $this->storeStamp(fn (StampService $service) => $service->endBreak());
     }
 
-    /** 認証ユーザと現在日時で StampService を作成する。 */
-    private function stamper(): StampService
+    /**
+     * 打刻処理を行う。
+     * 
+     * @param callable(StampService):void $callback
+     * @return RedirectResponse
+     */
+    private function storeStamp(callable $callback): RedirectResponse
     {
-        return App::call(function (DateTimeZone $timezone) {
-            return new StampService(Auth::user(), CarbonImmutable::now($timezone));
-        });
+        $now = CarbonImmutable::now(app()->make(DateTimeZone::class));
+        $service = new StampService(Auth::user(), $now);
+        $callback($service);
+        return redirect()->route('stamp');
     }
 }
