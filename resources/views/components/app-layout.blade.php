1dd840d95c0c1b4836b17060a3e1f6a7dc0f6292
ryamao <yamao.ryouta@gmail.com>
2024-02-02 22:45:02 +0900

    fix(view): safariとfirefoxで見た目が変わるため修正
    リセットCSSを変更
    safariだとfont-weight: boldが太くなりすぎるため、bold用のフォントを追加

--- resources/views/components/app-layout.blade.php
+++ resources/views/components/app-layout.blade.php
@@ -9,8 +9,8 @@
     <title>Atte</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@500;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}" />
     <link rel="stylesheet" href="{{ asset('css/app-layout.css') }}" />
     @isset($css)
     <link rel="stylesheet" href="{{ asset('css/' . $css) }}" />
