2a916fa82f74901edc99cf00014ee992cb58476b
ryamao <yamao.ryouta@gmail.com>
2024-02-06 21:20:23 +0900

    refactor: AttendanceRowとUserAttendanceRowの共通部分をトレイトにする

--- app/View/Components/AttendanceRow.php
+++ app/View/Components/AttendanceRow.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 namespace App\View\Components;
 
+use App\Traits\FormatTimeAndSeconds;
 use Closure;
-use DateTimeInterface;
-use DateTimeZone;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\View\Component;
@@ -14,10 +13,12 @@ use Illuminate\View\Component;
 /** 勤怠情報テーブルの行を表すコンポーネント */
 class AttendanceRow extends Component
 {
+    use FormatTimeAndSeconds;
+
     /**
      * Create a new component instance.
      */
-    public function __construct(private DateTimeZone $timezone, private Model $attendance)
+    public function __construct(private Model $attendance)
     {
         //
     }
@@ -34,25 +35,5 @@ class AttendanceRow extends Component
         $workSeconds = $this->formatSeconds($this->attendance->work_seconds);
 
         return view('components.attendance-row', compact('userName', 'shiftBegunAt', 'shiftEndedAt', 'breakSeconds', 'workSeconds'));
-    }
-
-    /** 開始日時や終了日時を表示形式に変換する。日時が null の場合は '--:--:--' を返す。 */
-    private function formatTime(?DateTimeInterface $datetime): string
-    {
-        if (is_null($datetime)) {
-            return '--:--:--';
-        }
-
-        return $datetime->format('H:i:s');
-    }
-
-    /** 休憩時間や勤務時間を表示形式に変換する。 */
-    private function formatSeconds(?int $seconds): string
-    {
-        if (is_null($seconds)) {
-            return '--:--:--';
-        }
-
-        return today($this->timezone)->addSeconds($seconds)->format('H:i:s');
     }
 }
